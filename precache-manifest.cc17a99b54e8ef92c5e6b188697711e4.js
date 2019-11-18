c4b030db384b70053e8d652d08d701fe9aabeb36
Maryna Tkachova <maritkacheva@hotmail.com>
2019-11-18 21:26:43 +0200

    Updates

---
+++ precache-manifest.cc17a99b54e8ef92c5e6b188697711e4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7836079b9b4d798040fe84e578b4e3ba",
+    "url": "/goit-react-hw-04-pet-store/index.html"
+  },
+  {
+    "revision": "343934a4ded38f7df2fe",
+    "url": "/goit-react-hw-04-pet-store/static/css/main.c2296409.chunk.css"
+  },
+  {
+    "revision": "b735568c669522702bf8",
+    "url": "/goit-react-hw-04-pet-store/static/js/2.cd366c2a.chunk.js"
+  },
+  {
+    "revision": "343934a4ded38f7df2fe",
+    "url": "/goit-react-hw-04-pet-store/static/js/main.6b57fc24.chunk.js"
+  },
+  {
+    "revision": "751d59c2e6c41bc27c14",
+    "url": "/goit-react-hw-04-pet-store/static/js/runtime~main.f333e608.js"
+  }
+]);
